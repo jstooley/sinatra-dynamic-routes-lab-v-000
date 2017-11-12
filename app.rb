1a87f3d76bd131dbe5249692c3611836f99ee9ce
Justin Tooley <jstooley823@gmail.com>
2017-11-12 16:21:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,8 +11,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
 
   end
-  get '/say/:word/word2/:word3/:word4/:word5' do
-    
+  get '/say/:word1/word2/:word3/:word4/:word5' do
+
   end
   get '/:operation/:number1/:number2'
 end
