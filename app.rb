c4a1c3588b9ed784182a6b2bff63ea351c468497
Justin Tooley <jstooley823@gmail.com>
2017-11-12 16:27:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
-     "#{:name}".reverse
+    name = :name
+     "#{name}".reverse
   end
   get '/square/:number' do
 
